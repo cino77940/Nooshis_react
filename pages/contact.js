78e3945a72a2216c15d0ec4c4b8c8f9b632b333e
cino <cuneyt.tai@gmail.com>
2022-09-09 10:13:39 +0200

    responsive update

--- pages/contact.js
+++ pages/contact.js
@@ -33,6 +33,13 @@ export default function About() {
                     <div className={styles.cell}>
                         <HiLocationMarker size={28} />
                         <p>5 Rue du Dr Arthur Petit, 77130 Montereau-Fault-Yonne</p>
+                        <iframe
+                            src="https://www.google.com/maps/embed?pb=!1m18!1m12!1m3!1d2649.7227273072094!2d2.9538750513157686!3d48.38506237914199!2m3!1f0!2f0!3f0!3m2!1i1024!2i768!4f13.1!3m3!1m2!1s0x47ef42cbec38e755%3A0x112078c3c2aff3c1!2s5%20Rue%20du%20Dr%20Arthur%20Petit%2C%2077130%20Montereau-Fault-Yonne!5e0!3m2!1sfr!2sfr!4v1662707007715!5m2!1sfr!2sfr"
+                            style={{ border: 0 }}
+                            allowFullScreen=""
+                            aria-hidden="false"
+                            tabIndex="0"
+                        />
                     </div>
                 </div>
 
